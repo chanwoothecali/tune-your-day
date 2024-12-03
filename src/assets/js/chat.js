25dbfd988e24111d68ad51736759d520d6f5e0fa
chanwoothecali <jungcali94@gmail.com>
2024-12-03 10:29:31 +0900

    채팅 기능 추가

--- src/assets/js/chat.js
+++ src/assets/js/chat.js
@@ -1,8 +1,8 @@
-const chatBox = document.querySelector('.chat-box');
-const chatInput = document.querySelector('.chat-input');
+const chatBox = document.querySelector('#chatContainer');
+const chatInput = document.querySelector('#submitAdvice');
 
-// const loadingSpinner = document.querySelector('.loading-spinner');
-const sendMessage = async () => {
+async function sendChatRequest() {
+
     const chatMessage = document.createElement('div');
     chatMessage.classList.add('ask');
     chatMessage.innerHTML = `${chatInput.value}`;
@@ -12,46 +12,37 @@ const sendMessage = async () => {
     const savedInputValue = chatInput.value;
     chatInput.value = '';
 
-    // 로딩 스피너 동적으로 추가
-    // chatBox.appendChild(loadingSpinner);
-    // loadingSpinner.style.display = 'flex';
+    try {
+        const response = await fetch('https://tune-your-day-openai.netlify.app/api/chat', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userMessage: savedInputValue
+            })
+        });
 
-    // 스크롤을 아래로 이동
-    // chatBox.scrollTop = chatBox.scrollHeight;
+        if (!response.ok){
+            throw new Error(response.statusText);
+        }
 
-    const response = await fetch('https://tune-your-day-openai.netlify.app/api/chat', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            // dateTimeString: dateTimeString,
-            // selectZodiacString: selectZodiacString,
-            userMessage: savedInputValue
-        })
-    });
+        const data = await response.json();
 
-    // 로딩 스피너 제거
-    // chatBox.removeChild(loadingSpinner);
+        console.log('data', data);
 
-    const data = await response.json();
+        const chatMessage = document.createElement('div');
+        chatMessage.classList.add('answer');
+        chatMessage.innerHTML = `${data.assistant}`;
+        chatBox.appendChild(chatMessage);
 
-    const astrologerMessage = document.createElement('div');
-    astrologerMessage.classList.add('answer');
-    astrologerMessage.innerHTML = `${data.assistant}`;
-    chatBox.appendChild(astrologerMessage);
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-    // 스크롤을 아래로 이동
-    // chatBox.scrollTop = chatBox.scrollHeight;
-};
-
-
-// Send 버튼을 클릭했을 때
-document.querySelector('#chat-submit').addEventListener('click', sendMessage);
-
-// 키보드의 엔터키를 눌렀을 때
 chatInput.addEventListener('keydown', (event) => {
     if (event.keyCode === 13 || event.key === 'Enter') {
-        sendMessage();
+        sendChatRequest();
     }
 });
